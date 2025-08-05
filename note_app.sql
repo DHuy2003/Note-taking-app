f87beac40a12a5f8a7066e6f8790837763ecc828
DHuy2003 <duchuy07062003@gmail.com>
2025-08-05 16:21:55 +0700

    Update note_app.sql

--- note_app.sql
+++ note_app.sql
@@ -67,37 +67,6 @@ CREATE TABLE charts (
     foreign key (note_id) references notes(note_id) on delete cascade
 );
 
-CREATE TABLE charts_data (
-    id int auto_increment primary key,
-    chart_id int NOT NULL,
-    label varchar(200),
-    value varchar(100),
-    color varchar(100),
-    created_at timestamp default CURRENT_TIMESTAMP,
-    updated_at timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    foreign key (chart_id) references charts(chart_id) on delete cascade
-);
 
-CREATE TABLE data_exports (
-    id int auto_increment primary key,
-    user_id int NOT NULL,
-    export_time timestamp default CURRENT_TIMESTAMP,
-    file_name varchar(255),
-    file_path varchar(255),
-    created_at timestamp default CURRENT_TIMESTAMP,
-    updated_at timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    foreign key(user_id) references users(user_id) on delete cascade
-);
-
-CREATE TABLE data_imports (
-    id int auto_increment primary key,
-    user_id int NOT NULL,
-    import_time timestamp default CURRENT_TIMESTAMP,
-    file_name varchar(255),
-    file_path varchar(255),
-    created_at timestamp default CURRENT_TIMESTAMP,
-    updated_at timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    foreign key(user_id) references users(user_id) on delete cascade
-);
 
 
